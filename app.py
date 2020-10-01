5ded500aba56b0f3f0417e07e4c354414e9977e5
Utkarsh Mall <utkarshmall247@gmail.com>
2020-10-01 18:04:28 +0530

    Update app.py
    Added a function which is called again and again to make calculations fast, and user can stop it by pressing 0.

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+def calc():
+    ''' This is a functiion which when called, perform calculation tasks which user wants '''
 
-num1 = float(input("Enter first number: "))
-op = input("Enter operator: ")
-num2 = float(input("Enter second number: "))
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator: ")
+    num2 = float(input("Enter second number: "))
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
-else:
-    print("Invalid operator")
+    if op == "+" :
+        print(num1 + num2)
+    elif op == "-" :
+        print(num1 - num2)
+    elif op == "/" :
+        print(num1 / num2)
+    elif op == "*" :
+        print(num1 * num2)
+    elif op == "**" :
+        print(num1 ** num2)
+    else:
+        print("Invalid operator")
+
+while True:
+    i = int(input("Press 1 to Continue, otherwise press 0\n")) 
+    if i == 1:
+        calc()
+    else:
+        break
+
